b99e8cb6e5aab0444bd000bf6ed7605921d09a5c
MajesticThrust <majesticthrust@gmail.com>
2020-09-16 19:24:11 +0300

    Basic login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'software-testing-task1';
+  public loginForm: FormGroup;
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      login: new FormControl('', [Validators.required]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+
+    this.loginForm.valueChanges.subscribe((changes) =>
+      console.log('Form changes', this.loginForm)
+    );
+  }
+
+  public onSubmit() {
+    console.log('Submitting form', this.loginForm);
+  }
 }
